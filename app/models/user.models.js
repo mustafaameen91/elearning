1203f00971361cd435eec26c2b94123be44a3d06
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2022-03-17 19:56:19 +0300

    status

--- app/models/user.models.js
+++ app/models/user.models.js
@@ -81,6 +81,14 @@ User.login = async (user, result) => {
       });
       console.log(loginUser);
 
+      console.log(
+         "password :",
+         CryptoJS.AES.decrypt(
+            loginUser[0].password,
+            process.env.SECRET_KEY
+         ).toString(CryptoJS.enc.Utf8)
+      );
+
       if (loginUser.length > 0) {
          console.log(
             CryptoJS.AES.decrypt(
@@ -89,12 +97,6 @@ User.login = async (user, result) => {
             ).toString(CryptoJS.enc.Utf8)
          );
 
-         console.log(
-            CryptoJS.AES.decrypt(
-               loginUser[0].password,
-               process.env.SECRET_KEY
-            ).toString(CryptoJS.enc.Utf8)
-         );
          if (
             CryptoJS.AES.decrypt(
                loginUser[0].password,
