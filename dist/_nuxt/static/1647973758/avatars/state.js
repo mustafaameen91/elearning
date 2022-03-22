352d83dca4d18b5adb5dfb01563832bb52e48b64
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2022-03-22 21:29:44 +0300

    add class

---
+++ dist/_nuxt/static/1647973758/avatars/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647973758",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Favatars",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
